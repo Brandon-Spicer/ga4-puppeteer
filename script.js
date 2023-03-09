5ee05571a5ffff8a1f0c0694e28c16ebde78a0f9
Brandon Spicer <brandon.david.spicer@gmail.com>
2023-03-09 04:05:52 -0800

    google signin, create new dimension in GA

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
+const puppeteerExtra = require("puppeteer-extra");
+const stealthPlugin = require("puppeteer-extra-plugin-stealth");
 const puppeteer = require("puppeteer");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 (async () => {
-  const browser = await puppeteer.launch();
+  puppeteerExtra.use(stealthPlugin());
+  const browser = await puppeteerExtra.launch({
+    headless: false,
+    executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+  });
+  // const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1280, height: 800 });
-  await page.goto("https://www.google.com/");
-  await new Promise((r) => setTimeout(r, 3000));
-  await page.screenshot({ path: "google.png", fullPage: false });
-  await browser.close();
+
+  await page.goto("https://accounts.google.com/signin/v2/identifier");
+  await page.type('[type="email"]', process.env.gmailUsername);
+  await page.click("#identifierNext");
+  await page.waitForTimeout(2500);
+
+  await page.type('[type="password"]', process.env.password);
+
+  await page.click("#passwordNext");
+
+  await page.waitForTimeout(2500);
+
+  await page.goto(
+    "https://analytics.google.com/analytics/web/#/a248550860p341008177/admin/customdefinitions/hub"
+  );
+
+  await page.waitForTimeout(2500);
+
+  await page.click('[guidedhelpid="create-custom-dimension"]');
+
+  await page.waitForTimeout(1000);
+  await page.type("#mat-input-0", "test");
+  await page.waitForTimeout(1000);
+  await page.type("#mat-input-2", "testini");
+  await page.waitForTimeout(1000);
+  await page.click(".definition-builder-button");
+  await page.waitForTimeout(1000);
+
+  await page.waitForTimeout(55000);
+
+  // await browser.close();
 })();
