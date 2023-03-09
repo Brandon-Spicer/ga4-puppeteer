d7d5d7a611d1b9f131ac589465d88f90d094c4f4
Brandon Spicer <brandon.david.spicer@gmail.com>
2023-03-09 01:26:24 -0800

    screenshot example

---
+++ script.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto("https://www.google.com/");
+  await new Promise((r) => setTimeout(r, 3000));
+  await page.screenshot({ path: "google.png", fullPage: false });
+  await browser.close();
+})();
